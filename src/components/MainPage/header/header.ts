7559524d81090e67b400bbb6ac0ee8c312676396
rdaniliuk <r.daniliuk.22@gmail.com>
2022-08-19 01:55:01 +0200

    fix: fix eslint bug

--- src/components/MainPage/header/header.ts
+++ src/components/MainPage/header/header.ts
@@ -1,1 +1,23 @@
+export default class HeaderRender {
+  template: string;
 
+  constructor() {
+    this.template = `
+         <div class="header">
+         <div class="header__logo logo">
+            <a href="##"><img src="../img/mainPageLogo.svg" alt="logo"></a>
+         </div>
+         <div class="header__login logo">
+            <img src="../img/loginLogo.svg" alt="loginLogo">
+         </div>
+         </div>
+        `;
+  }
+
+  render() {
+    const { body } = document;
+    const nav = document.createElement('nav');
+    nav.innerHTML = this.template;
+    body.prepend();
+  }
+}
