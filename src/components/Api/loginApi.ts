42d3de4f977543e00f89b346232a2e71fcdf6f34
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-08-19 17:10:53 +0300

    feat: add getNewToken API function

--- src/components/Api/loginApi.ts
+++ src/components/Api/loginApi.ts
@@ -1,22 +1,8 @@
 import BASE_URL from './urlApi';
-import { IUserLogin } from './userApi';
+import { IUserLogin, ILoginResp, ITokenResp } from './userApi';
 
 const LOGIN_URL = `${BASE_URL}/signin`;
 const OK = 200;
-
-export interface ITokenResp {
-  message: string;
-  token: string;
-  refreshToken: string;
-  userId: string;
-  name?: string;
-}
-
-export interface ILoginResp {
-  isSuccess: boolean;
-  tokenResp: ITokenResp;
-  errMsg: string;
-}
 
 export const loginUser = async (user: IUserLogin): Promise<ILoginResp> => {
   const resp = await fetch(LOGIN_URL, {
@@ -39,7 +25,7 @@ export const loginUser = async (user: IUserLogin): Promise<ILoginResp> => {
   };
 
   if (resp.status === OK) {
-    const data = await resp.json() as ITokenResp;
+    const data = (await resp.json()) as ITokenResp;
 
     loginResp.isSuccess = true;
     Object.assign(loginResp.tokenResp, data);
@@ -49,3 +35,5 @@ export const loginUser = async (user: IUserLogin): Promise<ILoginResp> => {
 
   return loginResp;
 };
+
+export default loginUser;
--- src/components/Api/userApi.ts
+++ src/components/Api/userApi.ts
@@ -21,6 +21,20 @@ export interface IUserResp {
   errMsg: string;
 }
 
+export interface ITokenResp {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userId: string;
+  name?: string;
+}
+
+export interface ILoginResp {
+  isSuccess: boolean;
+  tokenResp: ITokenResp;
+  errMsg: string;
+}
+
 export const createUser = async (newUser: IUserLogin): Promise<IUserResp> => {
   const resp = await fetch(USERS_URL, {
     method: 'POST',
@@ -40,7 +54,7 @@ export const createUser = async (newUser: IUserLogin): Promise<IUserResp> => {
   };
 
   if (resp.status === OK) {
-    const data = await resp.json() as IUser;
+    const data = (await resp.json()) as IUser;
 
     userResp.isSuccess = true;
     Object.assign(userResp.user, data);
@@ -49,4 +63,36 @@ export const createUser = async (newUser: IUserLogin): Promise<IUserResp> => {
   }
 
   return userResp;
+};
+
+export const getNewToken = async (userId: string, refreshToken: string): Promise<ILoginResp> => {
+  const resp = await fetch(`${USERS_URL}/${userId}/tokens`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  const loginResp : ILoginResp = {
+    isSuccess: false,
+    tokenResp: {
+      message: '',
+      token: '',
+      refreshToken: '',
+      userId: '',
+    },
+    errMsg: '',
+  };
+
+  if (resp.status === OK) {
+    const data = (await resp.json()) as ITokenResp;
+
+    loginResp.isSuccess = true;
+    Object.assign(loginResp.tokenResp, data);
+  } else {
+    loginResp.errMsg = await resp.text();
+  }
+
+  return loginResp;
 };
