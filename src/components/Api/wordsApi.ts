2336170511e99140ae5f394f28ca015ec066447a
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-08-19 22:44:16 +0300

    feat: add getWord API function

--- src/components/Api/wordsApi.ts
+++ src/components/Api/wordsApi.ts
@@ -26,6 +26,12 @@ export interface IWordsResp {
   errMsg: string;
 }
 
+export interface IWordResp {
+  isSuccess: boolean;
+  word: IWord;
+  errMsg: string;
+}
+
 export const getWords = async (group: number, page: number): Promise<IWordsResp> => {
   const resp = await fetch(`${WORDS_URL}?group=${group}&page=${page}`, {
     method: 'GET',
@@ -50,4 +56,45 @@ export const getWords = async (group: number, page: number): Promise<IWordsResp>
   }
 
   return wordsResp;
+};
+
+export const getWord = async (wordId: string): Promise<IWordResp> => {
+  const resp = await fetch(`${WORDS_URL}/${wordId}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  const wordResp : IWordResp = {
+    isSuccess: false,
+    word: {
+      id: '',
+      group: 0,
+      page: 0,
+      word: '',
+      image: '',
+      audio: '',
+      audioMeaning: '',
+      audioExample: '',
+      textMeaning: '',
+      textExample: '',
+      transcription: '',
+      wordTranslate: '',
+      textMeaningTranslate: '',
+      textExampleTranslate: '',
+    },
+    errMsg: '',
+  };
+
+  if (resp.status === OK) {
+    const data = (await resp.json()) as IWord;
+
+    wordResp.isSuccess = true;
+    Object.assign(wordResp.word, data);
+  } else {
+    wordResp.errMsg = await resp.text();
+  }
+
+  return wordResp;
 };
