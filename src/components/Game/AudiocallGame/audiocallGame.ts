b268ebe911c5f98122808ad7a9e6aa212d955873
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-09-01 21:41:09 +0300

    feat: add footer hiding on game start and hover on answers

--- src/components/Game/AudiocallGame/audiocallGame.ts
+++ src/components/Game/AudiocallGame/audiocallGame.ts
@@ -1,6 +1,7 @@
 import './audiocallGame.css';
 import { renderIntro } from './render';
 import { setStartButtonHandler } from './events';
+import { footerHidden } from '../footerHidden';
 
 const OUTPUT_AREA_SELECTOR = '.screen';
 export const appOutput = document.createElement('div');
@@ -14,6 +15,7 @@ const audiocallStart = (isStartedFromTextbook = false, group = 1, page = 1) => {
   const screen = document.querySelector(OUTPUT_AREA_SELECTOR);
   if (!screen) throw new Error('Error in HTML');
 
+  footerHidden();
   appOutput.classList.add('acg__output');
   screen.innerHTML = '';
   screen.append(appOutput);
