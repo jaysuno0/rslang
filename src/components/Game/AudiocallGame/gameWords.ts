3a099ac102d03181e2c067057916a5aec3e9b062
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-09-04 18:49:09 +0300

    feat: add words getting for logged user

--- src/components/Game/AudiocallGame/gameWords.ts
+++ src/components/Game/AudiocallGame/gameWords.ts
@@ -1,9 +1,13 @@
-import { getWords } from '../../Api/wordsApi';
+import { getWords, IWordsResp } from '../../Api/wordsApi';
+import { getUserAggregatedWords, IWordsParams, GET_HARD } from '../../Api/userAggregatedWords';
 import store from './gameStore';
 import { renderWordsLoading, renderErrMsg } from './render';
 import { getRandomNumber } from './utils';
+import state from '../../../state';
 
 const PAGES = 29;
+const WORDS_PER_PAGE = 20;
+const HARD_GROUP = 7;
 
 export const getGameWords = async (
   isPageSetted: boolean,
@@ -12,15 +16,58 @@ export const getGameWords = async (
 ) => {
   renderWordsLoading(store.appOutput);
 
-  const settedPage = isPageSetted ? page - 1 : getRandomNumber(PAGES);
-  const wordsResp = await getWords(group - 1, settedPage);
+  let settedPage = isPageSetted ? page : getRandomNumber(PAGES) + 1;
 
-  if (!wordsResp.isSuccess) {
-    renderErrMsg(store.appOutput, wordsResp.errMsg);
+  if(!state.isUserLogged) {
+    const wordsResp = await getWords(group - 1, settedPage);
+
+    if (!wordsResp.isSuccess) {
+      renderErrMsg(store.appOutput, wordsResp.errMsg);
+      return;
+    }
+
+    store.words = wordsResp.words;
     return;
   }
 
-  store.words = wordsResp.words;
+  const params: IWordsParams = {
+    page: settedPage,
+    wordsPerPage: WORDS_PER_PAGE,
+  }
+
+  if (group === HARD_GROUP) {
+    params.filter = GET_HARD;
+  }
+  else {
+    params.group = group - 1;
+  }
+
+  do {
+    settedPage -= 1;
+    params.page = settedPage;
+    const wordsResp = await getUserAggregatedWords(
+      state.userId,
+      state.accessToken,
+      params,
+    );
+
+    if (!wordsResp.isSuccess) {
+      renderErrMsg(store.appOutput, wordsResp.errMsg);
+      return;
+    }
+
+    if(isPageSetted) {
+      wordsResp.words = wordsResp.words.filter((word) =>
+        (!word.userWord) || (!word.userWord.optional) || (!word.userWord.optional.isLearned)
+      );
+    }
+    store.words = store.words.concat(wordsResp.words);
+  }
+  while ((settedPage > 0) && (store.words.length < WORDS_PER_PAGE));
+
+  if (store.words.length > WORDS_PER_PAGE) {
+    store.words = store.words.slice(0, WORDS_PER_PAGE);
+  }
 };
 
 export default getGameWords;
